19fd1bc2ddfadf471736f909a20a51568560c597
Trevor Heath <trevormheath@gmail.com>
2023-02-27 11:38:11 -0700

    Add project files.

---
+++ Bookstore/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bookstore.Models;
+using Bookstore.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookstore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookstoreRepository repo;
+
+        public HomeController(IBookstoreRepository repository)
+        {
+            repo = repository;
+        }
+
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 10;
+            var books = new BooksViewModel
+            {
+                Books = repo.Books
+                .OrderBy(b => b.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    CurrentPage = pageNum,
+                    ProjectsPerPage = pageSize,
+                    TotalNumProjects = repo.Books.Count()
+                }
+            };
+
+
+
+            return View(books);
+        }
+    }
+}
---
+++ Bookstore/Infrastructure/PaginationTagHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Bookstore.Models.ViewModels;
+
+namespace Bookstore.Infrastructure
+{
+    [HtmlTargetElement("div", Attributes = "page-inf")]
+    public class PaginationTagHelper : TagHelper
+    {
+        //Dynamically create the page links for us
+
+        private IUrlHelperFactory uhf;
+
+        public PaginationTagHelper(IUrlHelperFactory temp)
+        {
+            uhf = temp;
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext vc { get; set; }
+
+        //different than the view context
+        public PageInfo PageInf { get; set; }
+        public string PageAct { get; set; }
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
+
+        public override void Process(TagHelperContext thc, TagHelperOutput tho)
+        {
+            IUrlHelper uh = uhf.GetUrlHelper(vc);
+
+            TagBuilder final = new TagBuilder("div");
+
+            for (int i = 1; i <= PageInf.TotalPages; i++)
+            {
+                TagBuilder tb = new TagBuilder("a");
+                tb.Attributes["href"] = uh.Action(PageAct, new { pageNum = i });
+
+                if (PageClassesEnabled)
+                {
+                    tb.AddCssClass(PageClass);
+                    tb.AddCssClass(i == PageInf.CurrentPage
+                        ? PageClassSelected : PageClassNormal);
+                }
+
+                tb.InnerHtml.Append(i.ToString());
+
+                final.InnerHtml.AppendHtml(tb);
+
+            }
+            tho.Content.AppendHtml(final.InnerHtml);
+        }
+    }
+}
